acf8d7675b6e1ce410540ff3a4c8d2ddf6752e6a
Aakash Kumar <gzatrop@gmail.com>
2025-04-25 01:45:29 +0530

    fix

--- farmkart/android/app/build.gradle.kts
+++ farmkart/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.farmkart"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.11718014"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
